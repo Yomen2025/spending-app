83eaedf60d5c4a352823328acddef82af32b7f54
Yomen2025 <pikyencan@gmail.com>
2025-08-24 16:35:20 -0400

    Update supabase.ts
    update supabaseUrl & supabaseKey

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://ppzmhcojvwmizabagcpz.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Database types
 export interface Trip {
